c40cb20f7eba6f1a7284c2e4bc917f50b08c8596
Ben Schedin <benschedin@gmail.com>
2021-10-02 21:34:14 -0700

    added gender statistics

--- project-files/student-performance/ui.R
+++ project-files/student-performance/ui.R
@@ -11,23 +11,11 @@ library(shiny)
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("distPlot")
-        )
+  mainPanel(
+    tabsetPanel(
+      tabPanel("Plot", plotOutput("plot")),
+      tabPanel("Summary", verbatimTextOutput("summary")),
+      tabPanel("Table", tableOutput("table"))
     )
+  )
 ))
