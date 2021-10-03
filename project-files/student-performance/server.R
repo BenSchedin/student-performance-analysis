f8f9953171c6f0730d45ca79f2cb37b179738385
Ben Schedin <49740457+BenSchedin@users.noreply.github.com>
2021-10-03 01:07:55 -0700

    started prototyping dashboard

--- project-files/student-performance/server.R
+++ project-files/student-performance/server.R
@@ -8,19 +8,87 @@
 #
 
 library(shiny)
+library(tidyverse)
+
+data <- read.csv("www/full.csv")
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-
-    output$distPlot <- renderPlot({
-
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
+    
+    # Density plots for both schools, class period 1
+    output$g1Density <- renderPlot({
+        
+        threshold <- input$perfThresh
+        
+        full %>% 
+            select(school, G1) %>% 
+            ggplot(aes(x=G1, fill=school)) +
+            geom_density(alpha=0.25) +
+            geom_vline(xintercept=threshold, color="red") +
+            xlim(0, 20) +
+            theme_bw() +
+            labs(title="1st Period Grades", x="Score", y="Density")
+        
+    })
+    
+    # Density plots for both schools, class period 2
+    output$g2Density <- renderPlot({
+        
+        threshold <- input$perfThresh
+        
+        full %>% 
+            select(school, G2) %>% 
+            ggplot(aes(x=G2, fill=school)) +
+            geom_density(alpha=0.25) +
+            geom_vline(xintercept=threshold, color="red") +
+            xlim(0, 20) +
+            theme_bw() +
+            labs(title="2nd Period Grades", x="Score", y="Density")
+        
+    })
+    
+    # Density plots for both schools, class period 3
+    output$g3Density <- renderPlot({
+        
+        threshold <- input$perfThresh
+        
+        full %>% 
+            select(school, G3) %>% 
+            ggplot(aes(x=G3, fill=school)) +
+            geom_density(alpha=0.25) +
+            geom_vline(xintercept=threshold, color="red") +
+            xlim(0, 20) +
+            theme_bw() +
+            labs(title="3rd Period Grades", x="Score", y="Density")
+        
+    })
+    
+    # Density plots for both schools, average of class periods 1-3
+    output$gMeanDensity <- renderPlot({
+        
+        threshold <- input$perfThresh
+        
+        full %>% 
+            select(school, gMean) %>% 
+            ggplot(aes(x=gMean, fill=school)) +
+            geom_density(alpha=0.25) +
+            geom_vline(xintercept=threshold, color="red") +
+            xlim(0, 20) +
+            theme_bw() +
+            labs(title="Average Grades (All Periods)", x="Score", y="Density")
+        
+    })
+    
+    # Data table for selected period
+    output$belowThreshTable <- renderDataTable({
+        
+        threshold <- input$perfThresh
+        
+        full %>%
+            select(everything()) %>%
+            filter(G3 <= threshold) %>%
+            as_tibble()
+        
     })
 
 })
--- project-files/student-performance/ui.R
+++ project-files/student-performance/ui.R
@@ -8,14 +8,31 @@
 #
 
 library(shiny)
+library(shinythemes)
+library(DT)
+
+data <- read.csv("www/full.csv")
 
 # Define UI for application that draws a histogram
-shinyUI(fluidPage(
-  mainPanel(
-    tabsetPanel(
-      tabPanel("Plot", plotOutput("plot")),
-      tabPanel("Summary", verbatimTextOutput("summary")),
-      tabPanel("Table", tableOutput("table"))
-    )
-  )
-))
+shinyUI(fluidPage(theme=shinytheme("superhero"),
+                  titlePanel("Academic Performance Analytics System"),
+                      navbarPage("Navigation",
+                                 
+                                 tabPanel(icon("home")),
+                                 
+                                 tabPanel("General",
+                                                sidebarPanel(sliderInput("perfThresh", "Performance Threshold", min=0, max=20, value=10), width=2),
+                                          
+                                                mainPanel(fluidRow(column(plotOutput("g1Density"), width=6),
+                                                                   column(plotOutput("g2Density"), width=6)),
+                                                          fluidRow(column(plotOutput("g3Density"), width=6),
+                                                                   column(plotOutput("gMeanDensity"), width=6))),
+                                          
+                                                fluidRow(column(DT::dataTableOutput("belowThreshTable"), width=12))
+                                          ),
+                                 
+                                 tabPanel("Tab2")
+                                 
+                                 )
+                  )
+        )
